2cbaed2646e79cbbda357bbc1cf7cb436cdfa894
Sakiba <sakibasadek@gmail.com>
2026-01-26 22:52:47 +0600

    variable

---
+++ basic/problem_solve.js
@@ -0,0 +1,30 @@
+// *****problem-3*****
+
+let num =25;
+
+let remainder = num % 5;
+
+console.log(remainder)
+
+
+// *****problem-2*****
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let d = 40;
+// let e = 33;
+
+// let sum = a + b + c + d + e;
+// let average = sum / 5;
+
+
+// console.log(average.toFixed(2));
+
+// *****probelm-1*****
+
+// let given=1000,cost=700;
+
+// let change = given-cost;
+
+// console.log(change);
