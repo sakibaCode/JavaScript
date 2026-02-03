8d7d63502502560622f51faff7da68b99c3fd0eb
Sakiba <sakibasadek@gmail.com>
2026-02-03 20:46:03 +0600

    problem solve

--- daily_practise/oddAverage.js
+++ daily_practise/oddAverage.js
@@ -1,13 +1,18 @@
+function oddAverage(arr) {
 
-function oddAverage(arr){
+    let sum = 0;
+    let count = 0;
 
-    let count = 0
-    for(let n of arr){
-        if(n%2===0){
-            n+=count
+    for (let n of arr) {
+        if (n % 2 !== 0) {
+            sum += n;
+            count++;
         }
     }
-    return count
+
+    if (count === 0) return 0;
+
+    return sum / count;
 }
 
-console.log(oddAverage([2,4,1,3,5]))
+console.log(oddAverage([2, 4, 1, 3, 5]));
